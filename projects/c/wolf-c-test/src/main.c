453bc34a51698293494bc8d16656370337b169af
Marcos Wolf <MGWOLF2002@GMAIL.COM>
2026-08-14 13:07:31 -0300

    Add development environment test projects

---
+++ projects/c/wolf-c-test/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 2;
+    int b = 3;
+    int result = a + b;
+
+    printf("Wolf Workstation C toolchain OK\n");
+    printf("Result: %d\n", result);
+
+    return 0;
+}
---
+++ projects/python/wolf-python-test/tests/test_math.py
@@ -0,0 +1,5 @@
+from wolf_python_test.math import add
+
+
+def test_add():
+    assert add(2, 3) == 5
